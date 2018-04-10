d6d3fc47dd20bb73c0cbaa4a19aa64a31c530182
keithpuzey <keithpuzey@users.noreply.github.com>
2018-04-10 16:34:12 +0100

    Update UI-Test2 .js

--- saucelabtests/UI-Test2 .js
+++ saucelabtests/UI-Test2 .js
@@ -1,7 +1,7 @@
-describe('CDBU Modern Software Factory UI Test - Win Environment', function() {
+describe('CDBU Modern Software Factory UI Test -  Environment', function() {
   it('should add a todo', function() {
     browser.ignoreSynchronization = true;
-    browser.get('http://win-preprod.cdbu.io/');
+    browser.get('http://preprod.cdbu.io/');
     element(by.css('[name="Test 2"]')).sendKeys('Selenium Test User');
     element(by.buttonText('Submit')).click();
     });
