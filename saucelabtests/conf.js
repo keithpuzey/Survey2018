0a8772a132397834c89562fe5078f4afe4f6a22a
keithpuzey <keithpuzey@users.noreply.github.com>
2018-04-16 17:16:42 +0100

    Update conf.js

--- saucelabtests/conf.js
+++ saucelabtests/conf.js
@@ -1,4 +1,4 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['_Protractor3.js','_Protractor1.js',_'Protractor2.js']
+  specs: ['Protractor3.js','Protractor1.js','Protractor2.js']
 };
