9d9f7b17f060e54f25100f22fb68663cf428e98d
keithpuzey <keithpuzey@users.noreply.github.com>
2018-04-16 15:39:38 +0000

    Update todo-spec.js

--- saucelabtests/todo-spec.js
+++ saucelabtests/todo-spec.js
@@ -2,7 +2,37 @@ describe('CDBU Modern Software Factory UI Test - Survey Environment', function()
   it('should add a todo', function() {
     browser.ignoreSynchronization = true;
     browser.get('http://survey.cdbu.io:3000');
-    element(by.css('[name="Name"]')).sendKeys('Selenium Test User');
-    element(by.buttonText('Submit')).click();
+    // URL: 
+element(by.css('input')).click();
+// URL: 
+element(by.css('input')).clear().sendKeys('Keith');
+// URL: 
+element(by.css('button')).click();
+// URL: 
+element(by.css('label:nth-of-type(1)>span:nth-of-type(1)>svg')).click();
+// URL: 
+element(by.css('label:nth-of-type(1)>input[name="answers"]')).click();
+// URL: 
+element(by.css('button>span:nth-of-type(1)>span')).click();
+// URL: 
+element(by.css('label:nth-of-type(3)>span:nth-of-type(2)')).click();
+// URL: 
+element(by.css('label:nth-of-type(3)>span:nth-of-type(1)>svg')).click();
+// URL: 
+element(by.css('label:nth-of-type(3)>input[name="answers"]')).click();
+// URL: 
+element(by.css('button')).click();
+// URL: 
+element(by.css('label:nth-of-type(4)>span:nth-of-type(1)>svg')).click();
+// URL: 
+element(by.css('label:nth-of-type(4)>input[name="answers"]')).click();
+// URL: 
+element(by.css('button>span:nth-of-type(1)>span')).click();
+// URL: 
+element(by.css('label:nth-of-type(3)>span:nth-of-type(1)>svg')).click();
+// URL: 
+element(by.css('label:nth-of-type(3)>input[name="answers"]')).click();
+// URL: 
+element(by.css('button>span:nth-of-type(1)>span')).click();
     });
 });
