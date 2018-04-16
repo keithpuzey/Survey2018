804311945092698ebe3e6c337d1a9b71ca029e12
cacddemo <cacddemo@gmail.com>
2018-04-16 16:08:26 +0000

    ARD-Update

---
+++ saucelabtests/_Protractor3.js
@@ -0,0 +1,25 @@
+describe('CDBU Modern Software Factory UI Test - Survey Environment', function() {
+  it('should add a todo', function() {
+    browser.ignoreSynchronization = true;    browser.get('http://survey.cdbu.io:3000');element(by.css('input')).click();// URL: 
+element(by.css('input')).clear().sendKeys('Keith');// URL: 
+element(by.css('button')).click();// URL: 
+element(by.css('label:nth-of-type(1)>span:nth-of-type(1)>svg')).click();
+// URL: 
+element(by.css('label:nth-of-type(1)>input[name="answers"]')).click();
+// URL: 
+element(by.css('button>span:nth-of-type(1)>span')).click();// URL: 
+element(by.css('label:nth-of-type(2)>span:nth-of-type(1)>svg')).click();
+// URL: 
+element(by.css('label:nth-of-type(2)>input[name="answers"]')).click();
+// URL: 
+element(by.css('button>span:nth-of-type(1)>span')).click();// URL: 
+element(by.css('label:nth-of-type(1)>span:nth-of-type(1)>svg')).click();
+// URL: 
+element(by.css('label:nth-of-type(1)>input[name="answers"]')).click();
+// URL: 
+element(by.css('button>span:nth-of-type(1)>span')).click();// URL: 
+element(by.css('label:nth-of-type(1)>span:nth-of-type(1)>svg')).click();
+// URL: 
+element(by.css('label:nth-of-type(1)>input[name="answers"]')).click();
+// URL: 
+element(by.css('button>span:nth-of-type(1)>span')).click();
