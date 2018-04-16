a685402cb54bd3965179f710f13a8d68260dd5bc
keithpuzey <keithpuzey@users.noreply.github.com>
2018-04-16 14:53:45 +0000

    Update todo-spec.js

--- saucelabtests/todo-spec.js
+++ saucelabtests/todo-spec.js
@@ -1,7 +1,7 @@
-describe('CDBU Modern Software Factory UI Test - xxx Environment', function() {
+describe('CDBU Modern Software Factory UI Test - Survey Environment', function() {
   it('should add a todo', function() {
     browser.ignoreSynchronization = true;
-    browser.get('http://<preprod_url>');
+    browser.get('http://survey.cdbu.io:3000');
     element(by.css('[name="Name"]')).sendKeys('Selenium Test User');
     element(by.buttonText('Submit')).click();
     });
