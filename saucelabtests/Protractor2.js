a37c9a4534f7e03cd729e681694c687ea273a515
keithpuzey <keithpuzey@users.noreply.github.com>
2018-04-17 09:29:49 +0100

    Remove Port number from URL

--- saucelabtests/Protractor2.js
+++ saucelabtests/Protractor2.js
@@ -1,6 +1,6 @@
 describe('CDBU Modern Software Factory UI Test - Survey Environment', function() {
   it('should add a todo', function() {
-    browser.ignoreSynchronization = true;    browser.get('http://survey.cdbu.io:3000');element(by.css('input')).click();// URL: 
+    browser.ignoreSynchronization = true;    browser.get('http://survey.cdbu.io');element(by.css('input')).click();// URL: 
 element(by.css('input')).clear().sendKeys('Keith');// URL: 
 element(by.css('button')).click();// URL: 
 element(by.css('label:nth-of-type(1)>span:nth-of-type(1)>svg')).click();
