59c1c53c7bd318a35029d789d8ef1422c29f4665
keithpuzey <keithpuzey@users.noreply.github.com>
2018-04-17 10:25:36 +0100

    Update Protractor1.js

--- saucelabtests/Protractor1.js
+++ saucelabtests/Protractor1.js
@@ -5,22 +5,22 @@ element(by.css('input')).clear().sendKeys('Keith');// URL:
 element(by.css('button')).click();// URL: 
 element(by.css('label:nth-of-type(2)>span:nth-of-type(1)>svg')).click();
 // URL: 
-element(by.css('label:nth-of-type(2)>input[name="answers"]')).click();
+element(by.css('label:nth-child(2)>input[name="answers"]')).click();
 // URL: 
 element(by.css('button>span:nth-of-type(1)>span')).click();// URL: 
 element(by.css('label:nth-of-type(2)>span:nth-of-type(1)>svg')).click();
 // URL: 
-element(by.css('label:nth-of-type(2)>input[name="answers"]')).click();
+element(by.css('label:nth-child(2)>input[name="answers"]')).click();
 // URL: 
 element(by.css('button>span:nth-of-type(1)>span')).click();// URL: 
 element(by.css('label:nth-of-type(2)>span:nth-of-type(1)>svg')).click();
 // URL: 
-element(by.css('label:nth-of-type(2)>input[name="answers"]')).click();
+element(by.css('label:nth-child(2)>input[name="answers"]')).click();
 // URL: 
 element(by.css('button>span:nth-of-type(1)>span')).click();// URL: 
 element(by.css('label:nth-of-type(2)>span:nth-of-type(1)>svg')).click();
 // URL: 
-element(by.css('label:nth-of-type(2)>input[name="answers"]')).click();
+element(by.css('label:nth-child(2)>input[name="answers"]')).click();
 // URL: 
 element(by.css('button>span:nth-of-type(1)>span')).click();
     });
