82ab4633dfa57359026437b93aa901c44f4c319b
keithpuzey <keithpuzey@users.noreply.github.com>
2018-04-17 10:07:31 +0100

    Add Screen size

--- saucelabtests/conf.js
+++ saucelabtests/conf.js
@@ -1,4 +1,5 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
+   browser.manage().window().setSize(1600, 1200),
   specs: ['Protractor3.js','Protractor1.js','Protractor2.js']
 };
