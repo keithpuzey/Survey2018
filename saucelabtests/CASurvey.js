7eb212912cab61be05efd8df62b80e1c21aa91ca
keithpuzey <keithpuzey@users.noreply.github.com>
2018-04-30 15:27:05 +0100

    Update CASurvey.js

--- saucelabtests/CASurvey.js
+++ saucelabtests/CASurvey.js
@@ -24,33 +24,9 @@ element(by.css('label:nth-of-type(4)>span:nth-of-type(1)>svg')).click();
 element(by.css('.css-wk8to2')).click();
 
     });
-});describe('CDBU Modern Software Factory UI Test - Survey Environment', function() {
-  it('Goto Home Page', function() {
-    browser.ignoreSynchronization = true;    browser.get('http://preprod.survey.cdbu.io');
-    element(by.css('input')).click();
-// URL: 
-element(by.css('input')).clear().sendKeys('Charles Buhk');
-// URL: 
-element(by.css('button')).click();
-// URL: 
-element(by.css('label:nth-of-type(2)>span:nth-of-type(1)>svg')).click();
-// URL: 
-element(by.css('.css-wk8to2')).click();
-// URL: 
-element(by.css('label:nth-of-type(2)>span:nth-of-type(1)>svg')).click();
-// URL: 
-element(by.css('.css-wk8to2')).click();
-// URL: 
-element(by.css('label:nth-of-type(3)>span:nth-of-type(1)>svg')).click();
-// URL: 
-element(by.css('.css-wk8to2')).click();
-// URL: 
-element(by.css('label:nth-of-type(4)>span:nth-of-type(1)>svg')).click();
-// URL: 
-element(by.css('.css-wk8to2')).click();
+});
 
-    });
-});describe('CDBU Modern Software Factory UI Test - Survey Environment', function() {
+describe('CDBU Modern Software Factory UI Test - Survey Environment', function() {
   it('Goto Home Page', function() {
     browser.ignoreSynchronization = true;    browser.get('http://preprod.survey.cdbu.io');
     element(by.css('input')).click();
